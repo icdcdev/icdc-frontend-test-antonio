7b2289fb58725ccb0ef29de7fad175a0ab38dde5
ANNTOX117 <antonio.r.lpz92@gmail.com>
2022-08-04 23:09:35 -0500

    add Subjects controller

--- application/controllers/Subjects.php
+++ application/controllers/Subjects.php
@@ -2,8 +2,19 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Subjects extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('SubjectModel');
+        $this->load->library('session');
+    }
+
     public function index()
     {
+        $subject = new SubjectModel();
+        $subjects_all = $subject->get_subjects_can_chose_user(1);
+        $subjects_chosen = $subject->get_subjects_chosen_user(1);
         $this->load_views("subject/subject_view");
     }
 
--- application/models/SubjectModel.php
+++ application/models/SubjectModel.php
@@ -1,6 +1,42 @@
 <?php
 
-class SubjectModel
+class SubjectModel extends CI_Model
 {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function get_subjects_can_chose_user($id){
+        $this->db->query(
+            "SELECT sub.* FROM subjects sub 
+                LEFT JOIN subjects_by_student ss ON sub.id = ss.subject_id 
+                LEFT JOIN student s ON s.id = ss.student_id 
+                WHERE sub.id NOT IN (
+                    SELECT sub.id FROM subjects sub 
+                        INNER JOIN subjects_by_student ss ON sub.id = ss.subject_id 
+                        INNER JOIN student s ON s.id = ss.student_id where s.id =$id ) "
+        );
+        $query = $this->db->get();
 
+        if ($query->num_rows()) {
+            return $query->result();
+        } else {
+            return false;
+        }
+    }
+
+    public function get_subjects_chosen_user($id){
+        $this->db->query(
+            "SELECT sub.* FROM subjects sub 
+                        INNER JOIN subjects_by_student ss ON sub.id = ss.subject_id 
+                        INNER JOIN student s ON s.id = ss.student_id where s.id =$id"
+        );
+        $query = $this->db->get();
+
+        if ($query->num_rows()) {
+            return $query->result();
+        } else {
+            return false;
+        }
+    }
 }
